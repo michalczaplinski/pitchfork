a96c0a757e03614248d698a2f13ef26640e4e09d
Michal Czaplinski <michal.czaplinski@gxp-brain.com>
2016-06-06 13:37:34 +0200

    Removed the tests for MultiReview as it looks that pitchfork has stopped using those. Now all reviews seem to be be "single" reviews, so we only need the Review class.

--- tests/test_pitchfork.py
+++ tests/test_pitchfork.py
@@ -33,35 +33,5 @@ class TestReview(unittest.TestCase):
         for input_key in input_dict.keys():
             self.assertEqual(output_dict[input_key], input_dict[input_key])
 
-class TestMultiReview(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        cls.multi_review = search('radiohead', 'ok computer')
-
-    def test_multi_review(self):
-        self.assertIsInstance(self.multi_review, MultiReview)
-
-    def test_multi_review_album(self):
-        self.assertEqual(self.multi_review.album(), "OK Computer: Collector's Edition")
-
-    def test_multi_review_artist(self):
-        self.assertEqual(self.multi_review.artist(), 'Radiohead')
-
-    def test_multi_review_label(self):
-        self.assertEqual(self.multi_review.label(), 'Capitol')
-
-    def test_multi_review_year(self):
-        self.assertEqual(self.multi_review.year(), '2009')
-
-    def test_multi_review_url(self):
-        self.assertEqual(self.multi_review.url, '/reviews/albums/12938-pablo-honey-collectors-edition-the-bends-collectors-edition-ok-computer-collectors-edition/')
-
-    def test_multi_review_to_json(self):
-        input_dict = self.multi_review._json_safe_dict()
-        output_dict = json.loads(self.multi_review.to_json())
-        for input_key in input_dict.keys():
-            self.assertEqual(output_dict[input_key], input_dict[input_key])
-
 if __name__ == '__main__':
     unittest.main()
