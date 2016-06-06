c38f1499547447e2b97ee69d8c92fb20f8ee906d
Michal Czaplinski <michal.czaplinski@gxp-brain.com>
2016-06-06 15:09:33 +0200

    removed python2 compatibility for now, as the api returns unicode and we need to do extra decoding to parse it in python2.

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ setup(
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.0',
         'Programming Language :: Python :: 3.1',
